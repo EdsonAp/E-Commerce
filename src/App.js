4920838096c0df83e9a34951f05ff7e72783ee94
Edson Aparicio <edson.aparicio@icloud.com>
2020-10-10 22:13:31 -0500

    structure fiels

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import Homepg from './homecomponent.jsx'
+import Homepg from './pages/homepage/homepage'
+import MenuItem from './components/menu-item/menu-tems/menu-item-component'
+import Directory from './components/directory/directory.jsx'
 
 function App() {
   return (
     <>
+    <Directory />
+      <MenuItem />
       <Homepg/>
     </>
   );
