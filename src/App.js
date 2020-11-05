837e0fffb7773b09175e2191e5b22908917c43bc
Edson Aparicio <edson.aparicio@icloud.com>
2020-11-04 22:19:49 -0600

    routing fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switc, Route } from 'react-router-dom'
-import Homepg from './pages/homepage/homepage'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import HomePage from './pages/homepage/homepage'
 
+const HatsPage= props =>{
+  return(
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <>
-      <Homepg/>
+    <Route exact path='/' component={HomePage}/>
+    <Route exact path='/hats' component={HatsPage}/>
     </>
 
   );
