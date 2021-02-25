8d219f5844f2c9d3fff856120f90700e56d6095c
Edson Aparicio <edson.aparicio@icloud.com>
2021-02-24 20:18:52 -0600

    removed svg from regitration

--- src/components/register/sigin-register/register.jsx
+++ src/components/register/sigin-register/register.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Signin from './signincomponent'
 import Signup from './signup'
 import './register-styles.scss'
-import { ReactComponent as Logo } from '../../../Assets/register.svg'
+
+
 
 const Registerlog = () =>(
     <>
     <div className='register-log-page'>
         <Signin/>
         <Signup/>
-        <Logo/>
     </div>
 
     </>
